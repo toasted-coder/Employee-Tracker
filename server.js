c8e9544c0d00281f62f5e0a43b458720472a6d65
toasted-coder <christmanholmes21@gmail.com>
2021-03-24 16:20:24 -0700

    removed some unnecessary dependencies and added the first inquirer prompt to server.js

--- server.js
+++ server.js
@@ -1,12 +1,63 @@
-const path = require('path');
-const express = require('express');
+//Importing dependencies and the connection object created in connection.js
 const mySQL = require('mySQL');
 const inquirer = require('inquirer');
+const connection = require('./config/connection');
 
+//Inquirer prompt
+const init = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View department',
+            'View role',
+            'View employee',
+            'Add department',
+            'Add role',
+            'Add employee',
+            'Update employee',
+            'Exit',
+        ],
+    })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'Add department':
+                    addDepartment();
+                    break;
 
-const app = express();
-const PORT = process.env.PORT || 3003
+                case 'Add role':
+                    addRole();
+                    break;
 
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-  });  
+                case 'Add employee':
+                    addEmployee();
+                    break;
+
+                case 'View department':
+                    viewDepartment();
+                    break;
+
+                case 'View role':
+                    viewRole();
+                    break;
+
+                case 'View employee':
+                    viewEmployee();
+                    break;
+
+                case 'Update employee':
+                    updateEmployee();
+                    break;
+
+                case 'Exit':
+                    connection.end();
+                    break;
+
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        });
+}
+
